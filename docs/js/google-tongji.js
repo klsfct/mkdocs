7e161138c36a60ef8de3b14618392870ac41a6b1
klsfct <742512055@qq.com>
2022-02-15 15:24:38 +0800

    增加谷歌统计

---
+++ docs/js/google-tongji.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LN71YMN310"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LN71YMN310');
+</script>
